2db5bcecb9433390838fbe95b5f4ec8a48ad18a7
ArtushKobalyan <artush.kobalyan16@gmail.com>
2025-01-05 14:32:40 +0400

    Update SplashActivity.java

--- app/src/main/java/com/example/start/SplashActivity.java
+++ app/src/main/java/com/example/start/SplashActivity.java
@@ -10,15 +10,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Установите макет для анимации
+        
         setContentView(R.layout.activity_splash);
 
-        // Задержка на 3 секунды
+        
         new Handler().postDelayed(() -> {
-            // После завершения анимации переходим на MainActivity
+            
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
             finish(); // Завершаем SplashActivity
-        }, 2400); // 3000 миллисекунд = 3 секунды
+        }, 2400); 
     }
 }
